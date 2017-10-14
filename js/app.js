b2c535b80f562e50263535ffebf2ba78692a8448
agatacslk <niemamadresu@yahoo.fr>
2017-10-14 16:19:19 +0200

    Zadanie 2 - pierwsze 'Więcej'

--- js/app.js
+++ js/app.js
@@ -3,19 +3,42 @@
 document.addEventListener('DOMContentLoaded', function() {
     
    var forDropdown = document.querySelector('.for-dropdown');
-    console.log(forDropdown);
     
     var dropdown = document.querySelector('.dropdown');
     
     forDropdown.addEventListener('mouseover', function() {
         dropdown.style.display = 'block';
+    
     });
     
     forDropdown.addEventListener('mouseout', function() {
         dropdown.style.display = 'none';
-        console.log(forDropdown);
+       
     });
 
+var buttons = document.querySelectorAll('.read-more');
+   console.log(buttons[0].previousElementSibling);
     
-        
+     buttons[0].addEventListener('click', function() {
+  
+      var textArea = this.previousElementSibling;
+         
+    
+         
+         this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+         
+          if (textArea.style.display === 'none' || textArea.style.display === '') {
+               textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+           textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      }
+              
+         
+      
+    });
+    
+    
+    
     });
